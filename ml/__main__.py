6b661f2d50347994acce860d9665886deb74289b
ayuryev <yuryev@yandex.ru>
2021-07-20 11:56:41 +0300

    case 1

--- ml/__main__.py
+++ ml/__main__.py
@@ -1,47 +1,10 @@
 import numpy as np
-from .stuff import sigmoid
+from .case1 import case1
 
 
 def main():
     print("Start ml")
-    training_inputs = np.array([[0, 0, 1],
-                                [1, 1, 1],
-                                [1, 0, 1],
-                                [0, 1, 1]])
-    training_outputs = np.array([[0, 1, 1, 0]]).T
-
-    np.random.seed(1)
-
-    synaptic_weights = 2 * np.random.random((3, 1)) - 1
-
-    print("Случайные стартовые веса")
-    print(synaptic_weights)
-
-    # Метод обратного распространения
-    for i in range(20000):
-        input_layer = training_inputs
-        outputs = sigmoid(np.dot(input_layer, synaptic_weights))
-
-        err = training_outputs - outputs
-        adjustments = np.dot(input_layer.T, err * (outputs * (1 - outputs)))
-        synaptic_weights += adjustments
-
-    print("Веса после обучение")
-    print(synaptic_weights)
-
-    print("Результат после обучения")
-    print(outputs)
-
-    # тест
-    new_inputs = np.array([1, 1, 0])  # new situation
-    outputs = sigmoid(np.dot(new_inputs, synaptic_weights))
-    print("Новая ситуация")
-    print(outputs)
-
-    new_inputs = np.array([0, 0, 1])  # new situation
-    outputs = sigmoid(np.dot(new_inputs, synaptic_weights))
-    print("Новая ситуация")
-    print(outputs)
+    case1()
 
 
 if __name__ == '__main__':
